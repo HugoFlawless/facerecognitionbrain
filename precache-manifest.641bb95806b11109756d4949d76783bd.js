bfe1ef7d81948f614d414f8aeca4c1233f7fc770
HugoNoWorries <hugonogueira8@gmail.com>
2020-04-20 00:01:16 +0100

    Updates

---
+++ precache-manifest.641bb95806b11109756d4949d76783bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178661b12a00153515e58ac9578a29d3",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "f5183a0d10e92fac65f7",
+    "url": "/facerecognitionbrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "84e6948aa8a12073d309",
+    "url": "/facerecognitionbrain/static/css/main.0e4d88d8.chunk.css"
+  },
+  {
+    "revision": "f5183a0d10e92fac65f7",
+    "url": "/facerecognitionbrain/static/js/2.14afd871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionbrain/static/js/2.14afd871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e6948aa8a12073d309",
+    "url": "/facerecognitionbrain/static/js/main.75f92722.chunk.js"
+  },
+  {
+    "revision": "cf6990c032c001c83906",
+    "url": "/facerecognitionbrain/static/js/runtime-main.fdc8abc9.js"
+  }
+]);
